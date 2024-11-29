9df171fa28a0c7ca4db32352d1bdd4cc47af9e51
x-long <liang_xaut@163.com>
2024-11-29 08:49:22 +0800

    更新

--- project/net_io_count_ui.py
+++ project/net_io_count_ui.py
@@ -1,15 +1,31 @@
+import os.path
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+from project.path_manager import PathManager
+
+background = os.path.join(PathManager.icon_dir, "bg.jpg")
 
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(130, 50)
-        Dialog.setStyleSheet("background:#0c0c0c")
-        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+
+        layout = QtWidgets.QVBoxLayout(Dialog)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.frame = QtWidgets.QFrame()
+        self.frame.setObjectName("frame")
+        self.frame.setStyleSheet("QFrame#frame{border-image: url('" + background.replace("\\", "/") + "');}")
+
+        layout.addWidget(self.frame)
+
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.frame)
         self.verticalLayout.setContentsMargins(0, 7, 0, 7)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtWidgets.QLabel(Dialog)
+
+        self.label = QtWidgets.QLabel(self.frame)
         font = QtGui.QFont()
         font.setFamily("黑体")
         font.setPointSize(10)
@@ -22,7 +38,7 @@ class Ui_Dialog(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
-        self.label_down = QtWidgets.QLabel(Dialog)
+        self.label_down = QtWidgets.QLabel(self.frame)
         font = QtGui.QFont()
         font.setFamily("黑体")
         font.setPointSize(10)
